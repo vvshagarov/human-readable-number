534285583aa5864f5a0070552894a11968101a37
vvshagarov <60472070+vvshagarov@users.noreply.github.com>
2024-08-14 21:14:13 +0400

    fix: code
    new version 2

--- src/index.js
+++ src/index.js
@@ -1,50 +1,20 @@
 module.exports = function toReadable (number) {
   if (number === 0) {
     return 'zero';
-  } else if (number < 10 && number >= 1) {
-    const ones = [
-      ['one', 'two', 'three', 'four', 'five'],
-      ['six', 'seven', 'eight', 'nine']
-    ];
-    return ones[0][number - 1] || ones[1][number - 6];
-  } else if (number < 20) {
-    const teens = [
-      ['ten', 'eleven', 'twelve', 'thirteen'],
-      ['fourteen', 'fifteen', 'sixteen', 'seventeen'],
-      ['eighteen', 'nineteen']
-    ];
-    return teens[Math.floor(number / 10)][number % 10];
-  } else if (number < 100) {
-    const tens = [
-      ['twenty', 'thirty', 'forty', 'fifty'],
-      ['sixty', 'seventy', 'eighty', 'ninety']
-    ];
-    return tens[Math.floor(number / 10)][number % 10];
-  } else if (number < 1000) {
-    const hundreds = [
-      'one hundred',
-      'two hundred',
-      'three hundred',
-      'four hundred',
-      'five hundred',
-      'six hundred',
-      'seven hundred',
-      'eight hundred',
-      'nine hundred'
-    ];
-    return hundreds[Math.floor(number / 100)] + (number % 100 ? ` ${toReadable(number % 100)}` : '');
-  } else if (number < 1000000) {
-    const thousands = [
-      'one thousand',
-      'two thousand',
-      'three thousand',
-      'four thousand',
-      'five thousand',
-      'six thousand',
-      'seven thousand',
-      'eight thousand',
-      'nine thousand'
-    ];
-    return thousands[Math.floor(number / 1000)] + (number % 1000 ? ` ${toReadable(number % 1000)}` : '');
   }
-};
+
+  const belowTwenty = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const belowHundred = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  function helper(num) {
+    if (num < 20) {
+      return belowTwenty[num];
+    } else if (num < 100) {
+      return belowHundred[Math.floor(num / 10)] + (num % 10 !== 0 ? '-' + belowTwenty[num % 10] : '');
+    } else {
+      return belowTwenty[Math.floor(num / 100)] + ' hundred' + (num % 100 !== 0 ? ' ' + helper(num % 100) : '');
+    }
+  }
+
+  return helper(number);
+}
