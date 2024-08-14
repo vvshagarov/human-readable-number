f60e7d339479adc4b332a91fd041d818ffa6f44e
vvshagarov <60472070+vvshagarov@users.noreply.github.com>
2024-08-14 21:12:07 +0400

    fix: code
    new version

--- src/index.js
+++ src/index.js
@@ -1,24 +1,50 @@
 module.exports = function toReadable (number) {
-    const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-  const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-
-  const tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-  if (number < 10) {
-    return ones[number];
+  if (number === 0) {
+    return 'zero';
+  } else if (number < 10 && number >= 1) {
+    const ones = [
+      ['one', 'two', 'three', 'four', 'five'],
+      ['six', 'seven', 'eight', 'nine']
+    ];
+    return ones[0][number - 1] || ones[1][number - 6];
   } else if (number < 20) {
-    return teens[number - 10];
+    const teens = [
+      ['ten', 'eleven', 'twelve', 'thirteen'],
+      ['fourteen', 'fifteen', 'sixteen', 'seventeen'],
+      ['eighteen', 'nineteen']
+    ];
+    return teens[Math.floor(number / 10)][number % 10];
   } else if (number < 100) {
-    const tensNum = Math.floor(number / 10);
-    const rest = number % 10;
-    return tens[tensNum] + ' ' + ones[rest];
+    const tens = [
+      ['twenty', 'thirty', 'forty', 'fifty'],
+      ['sixty', 'seventy', 'eighty', 'ninety']
+    ];
+    return tens[Math.floor(number / 10)][number % 10];
   } else if (number < 1000) {
-    const hundreds = Math.floor(number / 100);
-    const rest = number % 100;
-    if (rest === 0) {
-      return ones[hundreds] + ' hundred';
-    }
-    return ones[hundreds] + ' hundred and ' + toReadable(rest);
-  } else {
-    return 'Number is too big';
+    const hundreds = [
+      'one hundred',
+      'two hundred',
+      'three hundred',
+      'four hundred',
+      'five hundred',
+      'six hundred',
+      'seven hundred',
+      'eight hundred',
+      'nine hundred'
+    ];
+    return hundreds[Math.floor(number / 100)] + (number % 100 ? ` ${toReadable(number % 100)}` : '');
+  } else if (number < 1000000) {
+    const thousands = [
+      'one thousand',
+      'two thousand',
+      'three thousand',
+      'four thousand',
+      'five thousand',
+      'six thousand',
+      'seven thousand',
+      'eight thousand',
+      'nine thousand'
+    ];
+    return thousands[Math.floor(number / 1000)] + (number % 1000 ? ` ${toReadable(number % 1000)}` : '');
   }
-}
+};
